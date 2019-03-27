642e7af2db5a76f70da2375ce6d6e457ce443518
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-03-27 02:57:22 -0400

    got http server in rust to read http requests

---
+++ http server/http/target/debug/deps/http-6e84d50ba3349fd5.d
@@ -0,0 +1,5 @@
+/home/nicktheacker/Rust-Code/Rust-Sampel/Rust-Sample/http server/http/target/debug/deps/http-6e84d50ba3349fd5: src/main.rs
+
+/home/nicktheacker/Rust-Code/Rust-Sampel/Rust-Sample/http server/http/target/debug/deps/http-6e84d50ba3349fd5.d: src/main.rs
+
+src/main.rs:
---
+++ http server/http/target/debug/http.d
@@ -0,0 +1,1 @@
+/home/nicktheacker/Rust-Code/Rust-Sampel/Rust-Sample/http\ server/http/target/debug/http: /home/nicktheacker/Rust-Code/Rust-Sampel/Rust-Sample/http\ server/http/src/main.rs
