5a1d93b9e855e2b155ab77275aae0bee2778e48b
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-03-27 04:21:46 -0400

    trying to send custom http requests

--- http server/http/src/main.rs
+++ http server/http/src/main.rs
@@ -20,13 +20,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-    // responces
     let i = 0;
     let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("src/lib/hello.html").unwrap();
+    let http = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
 
     stream.read(&mut buffer).unwrap();
-    stream.write(response.as_bytes()).unwrap();
-
+    stream.write(http.as_bytes()).unwrap();
+    
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     stream.flush().unwrap();
