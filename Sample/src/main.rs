278314ef78a63d78490ae20b943ad540728d8f33
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-03-27 02:16:18 -0400

    trying to create an rust calc

--- Sample/src/main.rs
+++ Sample/src/main.rs
@@ -1,15 +1,23 @@
 use std::io;
-
+use std::ops::Add;
 fn main() {
     // VARS
-    let mut x ;
-    let mut y ;
+    let mut x =String::new();
+    let mut y =String::new();
     
     println!("Rust--Calulator");
-
-    let answer =x + y;
-
     
+    println!("enter x value");
+    io::stdin().read_line(&mut x)
+        .expect("Failed to read line");
+
+    println!("enter y value");
+    io::stdin().read_line(&mut y)
+        .expect("Failed to read line");
+
+    let _answer =x + y;   
+    
+    println!("{}",_answer);
     
 
 }
