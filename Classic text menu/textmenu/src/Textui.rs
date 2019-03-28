fd61a8b7d7d4806f100b993cfa7b7fa919a89283
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-03-28 06:23:52 -0400

    added perfect input scanner

--- Classic text menu/textmenu/src/Textui.rs
+++ Classic text menu/textmenu/src/Textui.rs
@@ -1,17 +1,21 @@
 extern crate input_stream;
 mod test;
+mod run;
 use std::io;
 use input_stream::InputStream;
 use test::selftest;
+use run::run;
 
 pub fn input(){
-      println!("pL");
-    
+      println!("Rust--Menu");
+      println!("1: run Self-test");
+      println!("2: run Run");
     let stdin = io::stdin();
     let mut input = InputStream::new(stdin.lock());
     let integer: i32 = input.scan().expect("An integer");
 
     if (integer == 1) {
-      println!("hello 1")
+      println!("Running self test");
+      selftest();
     }
 }
