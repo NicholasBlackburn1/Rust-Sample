d924c9f08dcfd345625ca0bf8725c599f48e43a6
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-03-28 03:40:26 -0400

    useing match statement

--- Classic text menu/textmenu/src/Textui.rs
+++ Classic text menu/textmenu/src/Textui.rs
@@ -2,8 +2,19 @@ use std::io::{stdin,stdout,Write};
 
 fn input(){
 
-      let mut _input=String::new();
+    let mut _input=String::new();
+    let _=stdout().flush();
 
       print!("Please Enter your choose:");
-      print!("")
+      print!("1: Start_Self_Test");
+      println!("2: Start_program");
+      println!("3: QUIT");
+     
+    io::stdin::().read_line(&mut _input).expect(“error: unable to read user input”);
+
+     match _input{
+         
+     }
+     
+
 }
