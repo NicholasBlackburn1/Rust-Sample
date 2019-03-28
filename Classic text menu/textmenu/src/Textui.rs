bb551f3568bae85c37d383c72af9d17d628aa2dd
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-03-28 05:00:20 -0400

    trying  to fix input scanner

--- Classic text menu/textmenu/src/Textui.rs
+++ Classic text menu/textmenu/src/Textui.rs
@@ -1,9 +1,18 @@
+extern crate std;
+mod test;
+use std::prelude::*;
+use test::selftest;
+
 pub fn input(){
+    
+    let mut reader = io::stdin();
+    let input = reader.read_line().ok().expect("Failed to read line");
+    let input_num: Option<int> = from_str(input.as_slice().trim());
 
       println!("Please Enter your choose:");
       println!("1: Start_Self_Test");
       println!("2: Start_program");
       println!("3: QUIT");
 
- }
+}
      
